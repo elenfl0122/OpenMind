db18f44b57379b69ff54b63717bb6baa7dd3de1e
이지수 <144462290+elenfl0122@users.noreply.github.com>
2024-03-12 13:01:40 +0900

    Update App.jsx

--- openmind/src/App.jsx
+++ openmind/src/App.jsx
@@ -9,7 +9,6 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
-      <Route path="/">
         <Route index element={<MainPage />} />
         <Route path="list">
           <Route index element={<ListPage />} />
